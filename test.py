e8504d43c909d7799ee6a5c7e9a1627c968edf0d
Quintin Weigand <quinnweigand178@gmail.com>
2025-05-29 12:40:54 -0400

    tic tac toe is working now just need to detect which player got the win

--- test.py
+++ test.py
@@ -7,9 +7,40 @@ def main():
     player2 = Player(2)
     moveCount = 1
 
-    
-    row, col = Board.convertNumPositionToIndex(1)
-    print(row, col)
+
+    while(not board.hasWinner()):
+        board.displayBoard()
+        if moveCount % 2 == 1:
+            playerMoveSquare = input("Player 1: Please enter a square to place your 'X' -> : ")
+            try:
+                playerMoveSquare = int(playerMoveSquare)
+                row, col = Board.convertNumPositionToIndex(playerMoveSquare)
+            except Exception as e:
+                print(f"Invalid input: {e}")
+                continue
+
+            if board.set(row, col, 'X'):
+                player1.addMove(playerMoveSquare)
+            else:
+                continue
+        elif moveCount % 2 == 0:
+            playerMoveSquare = input("Player 2: Please enter a square to place your 'O' -> : ")
+            try:
+                playerMoveSquare = int(playerMoveSquare)
+                row, col = Board.convertNumPositionToIndex(playerMoveSquare)
+            except Exception as e:
+                print(f"Invalid input: {e}")
+                continue
+
+            if board.set(row, col, 'O'):
+                player2.addMove(playerMoveSquare)
+            else:
+                continue
+        else:
+            raise RuntimeError("Panic: Something has gone terribly wrong!!!")
+        
+        moveCount += 1
+
     
 
 if __name__ == "__main__":
