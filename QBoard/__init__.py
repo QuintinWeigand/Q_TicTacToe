89562706d3af7ee66bfed211cd5bded951b935dd
Quintin Weigand <quinnweigand178@gmail.com>
2025-07-17 19:57:28 -0400

    somewhere?

--- QBoard/__init__.py
+++ QBoard/__init__.py
@@ -14,14 +14,14 @@ class QBoard:
             print(f"{i + 1} | {display_str}")
 
     def get_board(self):
-        board_representation = []
+        board_representation = {}
         for i in range(len(self.__q_board)):
             square = self.__q_board[i]
             particles = square.get_particle_list_copy()
             # Show subscripts and creation numbers
             display_str = ' '.join(f"{p.get_subscript()}[{p.get_creation_number()}]" for p in particles) if particles else '-'
-            board_representation.append(f"{i + 1} | {display_str}")
-        return "\n".join(board_representation)
+            board_representation[str(i + 1)] = display_str
+        return board_representation
 
     def add_player_move(self, board_position: int, move_number: int, creation_number: int):
         if board_position >= 1 and board_position <= 9:
